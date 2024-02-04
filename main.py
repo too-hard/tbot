6488d58d85f2910eaa131a4fd84fc679e313f046
Kater <47554020+too-hard@users.noreply.github.com>
2024-02-04 22:29:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from telebot import types
 import time
 from datetime import datetime, date, time
 
-bot = telebot.TeleBot('6740900511:AAG-C64U3wU18kkV5oSxfsLe2vLoP9oqoIQ')
+bot = telebot.TeleBot('token')
 button_foo = types.InlineKeyboardButton('Сегодня', callback_data='/today')
 button_bar = types.InlineKeyboardButton('Завтра', callback_data='/tomorrow')
 button_baz = types.InlineKeyboardButton('Неделя', callback_data='/week')
